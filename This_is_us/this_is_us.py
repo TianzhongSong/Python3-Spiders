1f87f661825a53090a5d18479fac0062d734b6de
Tianz <ssstz@outlook.com>
2018-02-06 21:09:11 +0800

    Update this_is_us.py

--- This_is_us/this_is_us.py
+++ This_is_us/this_is_us.py
@@ -13,12 +13,6 @@ def save_to_file(content):
         f.close()
 
 
-def get_proxy():
-    r = requests.get('http://127.0.0.1:5000/get')
-    proxy = BeautifulSoup(r.text, "lxml").get_text()
-    return proxy
-
-
 def get_comment_number(url):
     try:
         headers = {"User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 \
@@ -35,12 +29,11 @@ def get_comment_number(url):
         return None
 
 
-def get_one_page(url, proxy):
+def get_one_page(url):
     try:
         headers = {"User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 \
                                         (KHTML, like Gecko) Chrome/64.0.3282.119 Safari/537.36"}
-        proxies = {'http': proxy}
-        response = requests.get(url, headers=headers, proxies=proxies)
+        response = requests.get(url, headers=headers)
         if response.status_code == 200:
             return response.text
         else:
@@ -58,10 +51,10 @@ def parse_one_page(html):
             save_to_file(comment.strip())
 
 
-def main(start, proxy):
+def main(start):
     prefix = 'https://movie.douban.com/subject/26794215/comments?start='
     url = prefix + str(start*20) + '&limit=20&sort=new_score&status=P&percent_type='
-    html = get_one_page(url, proxy)
+    html = get_one_page(url)
     parse_one_page(html)
 
 
@@ -69,11 +62,7 @@ if __name__ == "__main__":
     url = 'https://movie.douban.com/subject/26794215/comments?status=P'
     total = get_comment_number(url)
     print('total comment number is:', total)
-    proxy = get_proxy()
     for i in range(total//20):
-        if i % 10 == 0:
-            proxy = get_proxy()
-            print('current ip:', proxy)
         sleep(1)
         print('Current page:', i)
-        main(i, proxy)
+        main(i)
